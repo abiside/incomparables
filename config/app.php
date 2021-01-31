ac2dd6778274941f64ce2b0c9096e7e96c94c2ae
Abimael Espinoza <abimael@lovlisoft.com>
2021-01-31 12:31:52 -0600

    Remove scrap service provider from the app. config

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Abiside\NewScrap\NewScrapServiceProvider::class,
 
         /*
          * Application Service Providers...
